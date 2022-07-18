6d084793932ed2216d92c3a4cde4c9df3fe34c93
Winnie Tran <wintran987@gmail.com>
2022-07-18 14:52:43 -0700

    edited dark mode style

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 let setDark = function(){
     let main = document.querySelector("main");
-    main.style.backgroundColor = "black";
+    main.style.backgroundColor = "#a9a9a9";
     main.style.color = "white";
+    main.style.borderColor = "white";
 
     let nav = document.querySelector("nav");
-    nav.style.backgroundColor = "black";
+    nav.style.backgroundColor = "#a9a9a9";
     nav.style.color = "white";
+
+    let body = document.querySelector("body");
+    body.style.backgroundColor = "#242526";
+
+    let link = document.querySelector("a.menuLink:link");
+    link.style.color = "white";
+
+    let linkclicked = document.querySelector("a.menuLink:visited");
+    linkclicked.style.color = "lightgray";
 }
 
 let dmButton = document.querySelector("button#darkMode");
